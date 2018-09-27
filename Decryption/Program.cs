06abee519c2f077774dea1c27ad0cc2b4714f732
Svetlana Reutskaya <lana.reutska@gmail.com>
2018-09-27 23:29:29 +0300

    add Vigenere decrypt

--- Decryption/Program.cs
+++ Decryption/Program.cs
@@ -16,15 +16,28 @@ namespace Decryption
 
         // 3. Maybe needed to refactor code
 
-        // 4. Write Vinegere Decryption
+        // 4. Write Vinegere Decryption - DONE
+
+        // 5. Write a code to attack some simple substitution cipher. 
+        //To reduce the complexity of this one we will use only uppercase letters, so the keyspace is only 26! 
+        //To get this one right automatically you will probably need to use some sort of genetic algorithm (which worked the best last year),
+        //simulated annealing or gradient descent. Seriously, write it right now, you will need it to decipher the next one as well. 
+        //Bear in mind, therea??s no spaces.
 
         static void Main(string[] args)
         {
+
+            #region Task1
             string task1 = @"]|d3gaj3r3avcvrgz}t>xvj3K\A3pzc{va=3V=t=3zg3`{|f.w3grxv3r3`gaz}t31{v..|3d|a.w13r" +
                 "}w?3tzev}3g{v3xvj3z`31xvj1?3k|a3g{v3uza`g3.vggva31{13dzg{31x1?3g{v}3k|a31v13dzg{31v1?3g{v}3" +
                 "1.13dzg{31j1?3r}w3g{v}3k|a3}vkg3p{ra31.13dzg{31x13rtrz}?3g{v}31|13dzg{31v13r}w3`|3|}=3J|f3~" +
                 "rj3f`v3z}wvk3|u3p|z}pzwv}pv?3[r~~z}t3wz`gr}pv?3Xr`z`xz3vkr~z}rgz|}?3`grgz`gzpr.3gv`g`3|a3d{" +
                 "rgveva3~vg{|w3j|f3uvv.3d|f.w3`{|d3g{v3qv`g3av`f.g=";
+            CeasarDecrypt ceasarDecrypt = new CeasarDecrypt(task1);
+            //ceasarDecrypt.Decrypt();
+            #endregion
+
+            #region Task2
 
             string task2 = "1c41023f564b2a130824570e6b47046b521f3f5208201318245e0e6b40022643072e13183e51183f" +
                 "5a1f3e4702245d4b285a1b23561965133f2413192e571e28564b3f5b0e6b50042643072e4b023f4a4b24554b3f5" +
@@ -36,22 +49,11 @@ namespace Decryption
                 "410a2f5a0e25474b2f56182856053f1d4b185619225c1e385f1267131c395a1f2e13023f13192254033f1305244" +
                 "4476b4a043e131c225f076b5d0e2e574b22474b3f5c4b2f56082243032e414b3f5b0e6b5d0e33474b245d0e6b52" +
                 "186b440e275f456b710e2a414b225d4b265a052f1f4b3f5b0e395689cbaa186b5d046b401b2a500e381d61";
-
-            CeasarDecrypt ceasarDecrypt = new CeasarDecrypt(task1);
-            //ceasarDecrypt.Decrypt();
-
             string result1 = DecodeHexToUTF8(task2);
+            VigenereDecrypt vigenereDecrypt = new VigenereDecrypt(result1, null);
+            vigenereDecrypt.Decrypt();
+            #endregion
 
-            // finding index of coidence
-
-            for (int i = 1; i < 50; i++)
-            {
-                LetterCounter lc = new LetterCounter(result1, i);
-                Console.WriteLine(i + " length " + lc.IndexOfCoidence());
-                //lc.Show();
-            }
-
-            // length maybe 3? or 6?
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
@@ -68,4 +70,5 @@ namespace Decryption
             return result;
         }
     }
+    
 }
---
+++ Decryption/VigenereDecrypt.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decryption
+{
+    public class VigenereDecrypt
+    {
+        public string source;
+        public int keyLength;
+        public int[] key;
+
+        public VigenereDecrypt(string source, int? keyLength)
+        {
+            this.source = source;
+            this.keyLength = keyLength!=null? Convert.ToInt32(keyLength): FindKeyLength();
+            key = new int[this.keyLength];
+        }
+
+        private int FindKeyLength()
+        {
+            int maxIndex = -1;
+            double indexOfCoidence = -1;
+            double curIndex;
+
+            for (int i = 1; i < 50; i++)
+            {
+                LetterCounter letterCounter = new LetterCounter(source, i);
+                curIndex = letterCounter.IndexOfConcidence();
+                if (curIndex > indexOfCoidence)
+                {
+                    if(curIndex/indexOfCoidence>1.4)
+                    {
+                        maxIndex = i;
+                        return maxIndex;
+                    }
+                    indexOfCoidence = letterCounter.IndexOfConcidence();
+                    maxIndex = i;
+                }
+            }
+            return maxIndex;
+        }
+
+        public void Decrypt()
+        {
+            key = new int[keyLength];
+            string result = string.Empty;
+
+            for (int i = 0; i < keyLength; i++)
+            {
+                for (int j = 0; j < 256; j++)
+                {
+                    result = string.Empty;
+
+                    for (int k = 0; k < source.Length - keyLength + 1; k += keyLength)
+                    {
+                        result += Convert.ToChar(source[k + i] ^ j);
+                    }
+
+                    int letterCount = 0;
+                    int spaceCount = 0;
+
+                    foreach (char c in result)
+                    {
+                        if (Char.IsLetter(c))
+                            letterCount++;
+                        if (Char.IsWhiteSpace(c))
+                            spaceCount++;
+                    }
+
+                    if ((((double)letterCount / result.Length) > 0.7)
+                        && (((double)spaceCount / result.Length) > 0.12)
+                        && (((double)(letterCount + spaceCount) / result.Length) > 0.95))
+                    {
+                        //Console.WriteLine(i + "  " + j);
+                        key[i] = j;
+                    }
+                }
+            }
+
+            result = string.Empty;
+
+            for (int i = 0; i < source.Length - keyLength + 1; i += keyLength)
+            {
+                for (int j = 0; j < keyLength; j++)
+                {
+                    result += Convert.ToChar(source[i + j] ^ key[j]);
+                }
+            }
+            Console.Write("key: ");
+            for (int i = 0; i < keyLength; i++)
+            {
+                Console.Write(Convert.ToChar(key[i]));
+            }
+            Console.WriteLine("\nresult string:\n" + result);
+
+        }
+    }
+}
