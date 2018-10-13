798499b56ea52678fc52c65c242c81070d35cf78
Svetlana Reutskaya <lana.reutska@gmail.com>
2018-10-13 18:19:41 +0300

    finall 4th

--- Decryption/SubstitutonPolyALphDecryptor.cs
+++ Decryption/SubstitutonPolyALphDecryptor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Decryption.SubstitutionDecript;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Decryption
 {
-    public class SimpleSubstitutionDecrypt
+    public class SubstitutonPolyALphDecryptor
     {
         public string source;
         public static Dictionary<string, double> trigrams;
@@ -19,10 +20,12 @@ namespace Decryption
         static int exiter2 = 0;
         static bool speedy2 = true;
         static bool speedy = true;
+        public int keyCount;
 
-        public SimpleSubstitutionDecrypt(string source)
+        public SubstitutonPolyALphDecryptor(string source, int keyCount)
         {
             this.source = source;
+            this.keyCount = keyCount;
         }
 
         public string Decrypt()
@@ -59,54 +62,74 @@ namespace Decryption
         public static string TransformByHillСlimbing(string text)
         {
             double coef = 0;
-            var key = GenerateRandomKey();
-            var tempKey = key;
-            var bestKey = tempKey;
+            var key1 = GenerateRandomKey();
+            var key2 = GenerateRandomKey();
+            var key3 = GenerateRandomKey();
+            var key4 = GenerateRandomKey();
+            var tempKey1 = key1;
+            var tempKey2 = key2;
+            var tempKey3 = key3;
+            var tempKey4 = key4;
+            var bestKey1 = tempKey1;
+            var bestKey2 = tempKey2;
+            var bestKey3 = tempKey3;
+            var bestKey4 = tempKey4;
             double bestKeyCoef = 0;
             int keyCounter = 0;
-            
+
             int exiter2 = 0;
             string temp = null;
 
-            while (exiter < 18)
+            while (exiter < 100)
             {
-                temp = TransformByFrequency(text, tempKey);
+                temp = TransformByFrequency(text, tempKey1, tempKey2, tempKey3, tempKey4);
 
-                //Console.SetCursorPosition(0, 0);
-                //Console.WriteLine(temp);
-                //Console.WriteLine("Coef    : " + coef + "             ");
-                //Console.WriteLine("Exiter  : " + exiter + "           ");
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(temp);
+                Console.WriteLine("Coef    : " + coef + "             ");
+                Console.WriteLine("Exiter  : " + exiter + "           ");
                 if (stage == 1)
                 {
-                    //Console.WriteLine("Stage   : " + stage + "           ");
-                    //Console.WriteLine("Exiter2 : " + exiter2 + "           ");
+                    Console.WriteLine("Stage   : " + stage + "           ");
+                    Console.WriteLine("Exiter2 : " + exiter2 + "           ");
                 }
                 else if (!recalculated)
                 {
-                    //Console.ForegroundColor = ConsoleColor.Green;
-                    //Console.WriteLine("Bigram stage Done");
-                    //Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Bigram stage Done");
+                    Console.ResetColor();
                 }
-
-                if (exiter2 >= 40)
+                CipherFitness fit = new CipherFitness();
+                
+                if (exiter2 >= 50)
                 {
                     stage = 2;
                     exiter = 0;
                     coef = GetCoefficient(temp, stage);
+                    //coef = fit.dictionaryStatisticFitness(temp);
                     exiter2 = 0;
                 }
-                var tempCoef = GetCoefficient(temp, stage);
+
+                //var tempCoef = GetCoefficient(temp, stage); 
+                double tempCoef;
+                tempCoef = GetCoefficient(temp, 2);
 
                 if (coef < tempCoef)
                 {
                     if (bestKeyCoef < tempCoef)
                     {
-                        bestKey = tempKey;
+                        bestKey1 = tempKey1;
+                        bestKey2 = tempKey2;
+                        bestKey3 = tempKey3;
+                        bestKey4 = tempKey4;
                         bestKeyCoef = tempCoef;
                     }
-                    if (keyCounter >= 100)
+                    if (keyCounter >= 20)
                     {
-                        key = tempKey;
+                        key1 = tempKey1;
+                        key2 = tempKey2;
+                        key3 = tempKey3;
+                        key4 = tempKey4;
                         coef = tempCoef;
                         exiter = 0;
                         exiter2 = 0;
@@ -122,7 +145,16 @@ namespace Decryption
                         exiter2++;
                 }
 
-                tempKey = ChangeKey(key);
+                var evolution = new Random().Next(1, 40);
+                //if (evolution < 5) tempKey1 = ChangeKey(key1);
+                //if(evolution>2 && evolution <6) tempKey2 = ChangeKey(key2);
+                //if (evolution > 3 && evolution < 7) tempKey3 = ChangeKey(key3);
+                //if (evolution > 6) tempKey4 = ChangeKey(key4);
+
+                if(evolution<17) tempKey1 = ChangeKey(key1);
+                if (evolution < 25) tempKey2 = ChangeKey(key2);
+                if (evolution < 20) tempKey3 = ChangeKey(key3);
+                if (evolution < 19) tempKey4 = ChangeKey(key4);
                 keyCounter++;
             }
 
@@ -135,18 +167,22 @@ namespace Decryption
             //переробити на правильне
             double res = 0;
 
-            var trigs = trigrams/*GetTrigrams()*/;
+            var bigs = bigrams;
+            var trigs = trigrams;
 
-            if (stage == 1)
-                trigs = bigrams;
-            else
-                trigs = trigrams;
+            foreach (var tg in bigs)
+            {
+                int counter = text.Split(new string[] { tg.Key }, StringSplitOptions.None).Count() - 1;
+                res += Math.Log10(tg.Value) * counter;
+            }
 
             foreach (var tg in trigs)
             {
                 int counter = text.Split(new string[] { tg.Key }, StringSplitOptions.None).Count() - 1;
                 res += Math.Log10(tg.Value) * counter;
             }
+
+            res += new CipherFitness().dictionaryStatisticFitness(text);
 
             return res;
         }
@@ -155,8 +191,8 @@ namespace Decryption
         {
             var list = input.ToList();
             Random random = new Random();
-            if (exiter2 > 20) speedy2 = false;
-            if (exiter > 8) speedy = false;
+            if (exiter2 > 30) speedy2 = false;
+            if (exiter > 100) speedy = false;
             if (stage == 1 && speedy2)
             {
                 int countSwap;
@@ -182,7 +218,7 @@ namespace Decryption
             else if (speedy)
             {
                 int countSwap;
-                countSwap = random.Next(1, 2);
+                countSwap = random.Next(1, 4);
 
                 int first;
                 int second;
@@ -202,12 +238,25 @@ namespace Decryption
             }
             else
             {
-                int first = random.Next(list.Count);
-                int second = random.Next(list.Count);
+                int countSwap;
+                countSwap = random.Next(1, 8);
+                //else countSwap = random.Next(1, 2);
 
-                var temp = list[first];
-                list[first] = list[second];
-                list[second] = temp;
+                int first;
+                int second;
+
+                List<int> listOfSwaps = new List<int>();
+                for (int i = 0; i < countSwap; i++)
+                {
+                    do
+                    {
+                        first = random.Next(list.Count);
+                        second = random.Next(list.Count);
+                    } while (listOfSwaps.Contains(first) || listOfSwaps.Contains(second));
+                    var temp = list[first];
+                    list[first] = list[second];
+                    list[second] = temp;
+                }
             }
 
             return list;
@@ -294,25 +343,68 @@ namespace Decryption
             return data;
         }
 
-        public static string TransformByFrequency(string text, IEnumerable<char> key)
+        public static string TransformByFrequency(string text, IEnumerable<char> key1, IEnumerable<char> key2, IEnumerable<char> key3, IEnumerable<char> key4)
         {
-            var abcByFreaquency = key;/* abc.OrderBy(x => x.Key).Select(x => x.Value);*/
+            var abcByFreaquency1 = key1;
+            var abcByFreaquency2 = key2;
+            var abcByFreaquency3 = key3;
+            var abcByFreaquency4 = key4;
             var textABCFreaquency = startDic;
+            char[] textCh = new char[1000000];
+            textCh = text.ToArray();
+            Stack<char> stack1 = new Stack<char>();
+            Stack<char> stack2 = new Stack<char>();
+            Stack<char> stack3 = new Stack<char>();
+            Stack<char> stack4 = new Stack<char>();
 
-            Stack<char> stack = new Stack<char>();
-
-            foreach (var i in abcByFreaquency)
+            foreach (var i in abcByFreaquency1)
             {
-                stack.Push(i);
+                stack1.Push(i);
+            }
+            foreach (var i in abcByFreaquency2)
+            {
+                stack2.Push(i);
+            }
+            foreach (var i in abcByFreaquency3)
+            {
+                stack3.Push(i);
+            }
+            foreach (var i in abcByFreaquency4)
+            {
+                stack4.Push(i);
             }
 
             foreach (var l in textABCFreaquency)
             {
-                text = text.Replace(l, stack.Pop());
+                //text = text.Replace(l, stack.Pop());
+                var res1 = stack1.Pop();
+                var res2 = stack2.Pop();
+                var res3 = stack3.Pop();
+                var res4 = stack4.Pop();
+
+                var rest = textCh.Length % 4;
+                for (int i = 3; i < textCh.Length; i+=4)
+                {
+                    if (textCh[i] == l) textCh[i] = res4;
+                    if (textCh[i-1] == l) textCh[i-1] = res3;
+                    if (textCh[i-2] == l) textCh[i-2] = res2;
+                    if (textCh[i-3] == l) textCh[i-3] = res1;
+                }
+                if(rest==1 && textCh[textCh.Length - 1] == l) textCh[textCh.Length-1] = res1;
+                else if (rest == 2)
+                {
+                    if (textCh[textCh.Length - 2] == l) textCh[textCh.Length - 2] = res1;
+                    if (textCh[textCh.Length - 1] == l) textCh[textCh.Length - 1] = res2;
+                }
+                else if (rest == 3)
+                {
+                    if (textCh[textCh.Length - 3] == l) textCh[textCh.Length - 3] = res1;
+                    if (textCh[textCh.Length - 2] == l) textCh[textCh.Length - 2] = res2;
+                    if (textCh[textCh.Length - 1] == l) textCh[textCh.Length - 1] = res3;
+                }
             }
 
-            return text;
+            return new string(textCh);
         }
-
     }
 }
